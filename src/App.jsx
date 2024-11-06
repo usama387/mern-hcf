76ad374b6247f3ae69bce7f311e37151dfccb178
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-06 10:28:31 +0500

    Installed TailwindCss and Shadcn ui and tested with a button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "./components/ui/button"
 
 const App = () => {
   return (
     <div>
-      <h2>Hi</h2>
+      <h2 className="text-emerald-500 text-4xl font-extrabold">Hi</h2>
+      <Button>Click Me</Button>
     </div>
   )
 }
