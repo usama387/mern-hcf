0e661aca4a807b86533c476bc6ad98d83cfd1154
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-08 11:08:55 +0500

    stored doctors from assets in a AppContextProvider and then wrapped entire app with it in main.jsx file to use doctors anywhere in the app

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,16 @@
+import { doctors } from "@/assets/assets";
+import { createContext } from "react";
+
+export const AppContext = createContext;
+
+const AppContextProvider = (props) => {
+  const value = {
+    doctors,
+  };
+
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
