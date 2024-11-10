c653c1fddfbbc93876c2d62aeaca94cdf69a8cb8
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-10 11:37:27 +0500

    Login Page which is dynamic based on the state Login or Sign Up completed

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,93 @@
+import { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [state, setState] = useState("Sign Up");
+
+  const [email, setEmail] = useState("");
+
+  const [password, setPassword] = useState("");
+
+  const [name, setName] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+  };
+  return (
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-[96px] rounded-xl text-gray-500 text-base shadow-lg">
+        <p className="bg-green-200 text-gray-800 w-max px-2 py-1 rounded-md text-xl font-semibold m-auto">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </p>
+        <p>
+          Please {state === "Sign Up" ? "sign up" : "login"} to schedule an
+          appointment
+        </p>
+
+        {/* sign up only renders when a new account is to be created */}
+        {state === "Sign Up" && (
+          <div className="w-full">
+            <p>Full Name</p>
+            <input
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              required
+              className="border border-zinc-300 rounded-lg w-full p-2 mt-1"
+            />
+          </div>
+        )}
+
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+            className="border border-zinc-300 rounded-lg w-full p-2 mt-1"
+          />
+        </div>
+
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            required
+            className="border border-zinc-300 rounded-lg w-full p-2 mt-1"
+          />
+        </div>
+        
+        {/* submit button logic */}
+        <button className="bg-blue-100 text-gray-500 w-full py-2 rounded-md text-base font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Login"}
+        </button>
+
+        {state === "Sign Up" ? (
+          <p>
+            Already have an account?{" "}
+            <span
+              className="text-blue-500 underline cursor-pointer"
+              onClick={() => setState("Login")}
+            >
+              Login Here
+            </span>
+          </p>
+        ) : (
+          <p>
+            Create a new account?{" "}
+            <span
+              className="text-blue-500 underline cursor-pointer"
+              onClick={() => setState("Sign Up")}
+            >
+              click here
+            </span>
+          </p>
+        )}
+      </div>
+    </form>
+  );
 };
 
 export default Login;
