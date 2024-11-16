bc28749d021460b1adb9c83436609df855775fa5
usama387 <128306049+usama387@users.noreply.github.com>
2024-11-16 18:56:57 +0500

    Fetched apis for both login and sign up based on the states dynamically and sent token to AppContext to access it anywhere

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,8 @@
-import { useState } from "react";
+import { AppContext } from "@/context/AppContext";
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [state, setState] = useState("Sign Up");
@@ -9,11 +13,55 @@ const Login = () => {
 
   const [name, setName] = useState("");
 
-  const onSubmit = async (event) => {
+  // destructuring data from app context
+  const { token, setToken, backendUrl } = useContext(AppContext);
+
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
+
+    try {
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/user/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.error);
+        }
+      } else {
+        const { data } = await axios.post(backendUrl + "/api/user/login", {
+          email,
+          password,
+        });
+
+        if (data.success) {
+          localStorage.setItem("token", data.token);
+          setToken(data.token);
+        } else {
+          toast.error(data.error);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
   };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
+
   return (
-    <form className="min-h-[80vh] flex items-center">
+    <form className="min-h-[80vh] flex items-center" onSubmit={onSubmitHandler}>
       <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-[96px] rounded-xl text-gray-500 text-base shadow-lg">
         <p className="bg-green-200 text-gray-800 w-max px-2 py-1 rounded-md text-xl font-semibold m-auto">
           {state === "Sign Up" ? "Create Account" : "Login"}
@@ -58,9 +106,12 @@ const Login = () => {
             className="border border-zinc-300 rounded-lg w-full p-2 mt-1"
           />
         </div>
-        
+
         {/* submit button logic */}
-        <button className="bg-blue-100 text-gray-500 w-full py-2 rounded-md text-base font-semibold">
+        <button
+          className="bg-blue-100 text-gray-500 w-full py-2 rounded-md text-base font-semibold"
+          type="submit"
+        >
           {state === "Sign Up" ? "Create Account" : "Login"}
         </button>
 
